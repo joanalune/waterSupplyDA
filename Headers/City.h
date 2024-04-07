7063ea18ff46398a3211f679977ccc26ec53a65d
Joana Noites <joanalune@gmail.com>
2024-04-07 18:21:04 +0100

    Added doxy comments for city class

--- Headers/City.h
+++ Headers/City.h
@@ -12,17 +12,68 @@ private:
     int cityId;
     double cityDemand;
 public:
+    /**
+     * City class constructor
+     * @param cityName
+     * @param cityCode
+     * @param cityPopulation
+     * @param cityId
+     * @param cityDemand
+     */
     City(string cityName, string cityCode, string cityPopulation, int cityId, double cityDemand);
+    /**
+     * City empty constructor
+     */
     City() = default;
+    /**
+     * Set a city's name
+     * @param name
+     */
     void setName(string name);
+    /**
+     * Set a city's code
+     * @param code
+     */
     void setCode(string code);
+    /**
+     * Set a city's population
+     * @param population
+     */
     void setPopulation(string population);
+    /**
+     * Set a city's ID
+     * @param id
+     */
     void setId(int id);
+    /**
+     * Set a city's demand
+     * @param demand
+     */
     void setDemand(double demand);
+    /**
+     * Get city's name
+     * @return
+     */
     string getName() const;
+    /**
+     * Get a city's code
+     * @return
+     */
     string getCode() const;
+    /**
+     * Get a city's population
+     * @return
+     */
     string getPopulation() const;
+    /**
+     * Get a city's ID
+     * @return
+     */
     int getId() const;
+    /**
+     * Get a city's demand
+     * @return
+     */
     double getDemand() const;
 };
 
